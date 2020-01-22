d7114689f46e682b9cf76cb7257dae08b28199d1
Nicolas PASCAL <np.pascal@gmail.com>
2020-01-22 10:49:50 +0100

    Update test_docker.sh

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -11,7 +11,7 @@ mkdir -p .cache/docker
 cd .cache/docker
 
 # create the project using the default settings in cookiecutter.json
-cookiecutter ../../ --no-input --overwrite-if-exists use_docker=y debug="y" 
+cookiecutter ../../ --no-input --overwrite-if-exists dockerize="runserver" debug="y" 
 cd my-shop
 ls
 
